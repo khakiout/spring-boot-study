da7dddc46dc85c75c484e718f8cda971b37d2643
Ruen Zapanta <rzapanta@grundfos.com>
2014-09-10 16:12:17 +0800

    added MVC post handler

--- src/main/java/hello/controller/WelcomeController.java
+++ src/main/java/hello/controller/WelcomeController.java
@@ -3,17 +3,27 @@ package hello.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WelcomeController {
 
-	@RequestMapping("/welcome")
+	@RequestMapping(value="/welcome", method=RequestMethod.GET)
 	public String welcome(
 			@RequestParam(value = "name", defaultValue = "Spring Boot") String name,
 			Model model) {
 		model.addAttribute("name", name);
 		
 		return "welcome";
+	}
+	
+	@RequestMapping(value="/welcome", method=RequestMethod.POST)
+	public String answer(
+			@RequestParam(value = "answer") String answer,
+			Model model) {
+		model.addAttribute("answer", answer);
+		
+		return "answer";
 	}
 }
