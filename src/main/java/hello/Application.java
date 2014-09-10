a7bc661eee3f158499ef8d89701218ce0b9504b6
Ruen Zapanta <rzapanta@grundfos.com>
2014-09-10 18:42:37 +0800

    commented out bean loop

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -15,7 +15,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(Application.class, args);
-
+		/*
 		System.out.println("Let's inspect the beans provided by Spring Boot.");
 
 		String[] beanNames = context.getBeanDefinitionNames();
@@ -23,6 +23,7 @@ public class Application {
 		for (String beanName : beanNames) {
 			System.out.println(beanName);
 		}
+		*/
 	}
 
 }
