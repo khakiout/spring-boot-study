6517bd907091c41c21fb11ea02dac308a9a029ba
Ruen Zapanta <rzapanta@grundfos.com>
2014-09-16 15:40:54 +0800

    initial study for application.properties

--- src/main/java/hello/controller/HelloController.java
+++ src/main/java/hello/controller/HelloController.java
@@ -1,13 +1,18 @@
 package hello.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	
+	@Value("${app.name}")
+	public String appName;
+	
 	@RequestMapping("/")
 	public String index() {
+		System.out.println(appName);
 		return "Hello World!";
 	}
 }
